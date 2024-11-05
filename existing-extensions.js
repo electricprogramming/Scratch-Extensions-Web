3d9a706c7da50d5d23e2b33bada33f39d15b074f
electricprogramming <yeungj316@gmail.com>
2024-11-05 18:11:06 -0500

    Create existing-extensions.js

---
+++ existing-extensions.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    name: 'Data Grids',
+    path: 'Data-Grids',
+    description: `Create and manage data grids in a similar way to Scratch's defaut variables category.`,
+    by: 'electricprogramming'
+  }
+];
