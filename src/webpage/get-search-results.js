2345e5d01d36d4cd5af6912269231913341af1c1
electricprogramming <yeungj316@gmail.com>
2024-11-06 18:36:33 -0500

    Create get-search-results.js

---
+++ src/webpage/get-search-results.js
@@ -0,0 +1,23 @@
+function getSearchResults(possibleResults, query) {
+  function checkMatch(text, query) {
+    var keywords = [...new Set(query.split(' '))];
+    keywords = keywords.filter(item => item !== '');
+    var count = 0;
+    keywords.forEach((keyword) => {
+      if (text.includes(keyword)) {
+        count += 1;
+      }
+    });
+    return count === keywords.length;
+  }
+  if (!(typeof possibleResults === 'object' && typeof query === 'string')) return [];
+  let results = [];
+  possibleResults.forEach(item => {
+    const text = item.toLowerCase();
+    if (checkMatch(text, query.toLowerCase())) {
+      results.push(item);
+    }
+  });
+  return results;
+}
+export default getSearchResults;
