df74527e39901c6c83c67977bc378c2a45a99d21
electricprogramming <yeungj316@gmail.com>
2024-11-06 18:52:21 -0500

    Update script.js

--- src/webpage/script.js
+++ src/webpage/script.js
@@ -1,1 +1,8 @@
-
+import extensions from '../existing-extensions.js';
+import getSearchResults from './get-search-results.js';
+extensions.forEach((extension) => {
+  const extButton = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  extButton.id = extension.path;
+  extButton.src = `../extension-icons/${extension.path}.svg`;
+  extButton.width = 300; extButton.height = 200;
+})
