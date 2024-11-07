c7f824da0aa954bbf93c8efbad10827704b1c08e
electricprogramming <yeungj316@gmail.com>
2024-11-07 08:20:20 -0500

    Update and rename script.js to index.js

---
+++ src/webpage/index.js
@@ -0,0 +1,18 @@
+import isMobile from './is-mobile.js';
+if (typeof window !== 'undefined') {
+  if (isMobile()) {
+    document.body.innerHTML = '';
+    fetch('/mobile/index.html')
+      .then(response => response.text())
+      .then(data => {
+        document.body.innerHTML = data;
+      });
+  } else {
+    document.body.innerHTML = '';
+    fetch('/desktop/index.html')
+      .then(response => response.text())
+      .then(data => {
+        document.body.innerHTML = data;
+      });
+  }
+}
