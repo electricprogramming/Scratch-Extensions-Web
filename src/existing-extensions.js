ecbc6d7e7a4fb7cdaeec05cc8bc8871991869adf
electricprogramming <yeungj316@gmail.com>
2024-11-09 10:56:33 -0500

    create data grids icon; update extisting-extensions.js

--- src/existing-extensions.js
+++
@@ -1,8 +0,0 @@
-export default [
-  {
-    name: 'Data Grids',
-    path: 'Data-Grids',
-    description: `Create and manage data grids in a similar way to Scratch's defaut variables category.`,
-    by: 'electricprogramming'
-  }
-];
---
+++ src/webpage/existing-extensions.js
@@ -0,0 +1,25 @@
+/* 
+This is a .js file instead of a .json file for a few reasons:
+  1) Not applicable in this case, but .js files can have comments.
+  2) `import` statements only work with .js files, not .json files.
+  3) More flexibility in notation -- JSON just doesn't look too nice to me.
+*/
+export default [
+  {
+    name: 'Data Grids',
+    keywords: [
+      'Data',
+      'Variable', 'Variables',
+      'Grid', 'Grids',
+      'List', 'Lists',
+      'Spreadsheet', 'Spreadsheets',
+      'Row', 'Rows',
+      'Column', 'Columns',
+      'Cell', 'Cells',
+      'Iteration'
+    ],
+    path: 'Data-Grids',
+    description: `Create and manage data grids in a similar way to Scratch's defaut variables category.`,
+    by: 'electricprogramming'
+  }
+];
