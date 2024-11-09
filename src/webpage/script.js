afcc1192c87e6d9e2dcf58e5c3a8dd55a8ce0a42
electricprogramming <yeungj316@gmail.com>
2024-11-09 08:44:58 -0500

    refresh the whole page instead of just the body

--- src/webpage/script.js
+++ src/webpage/script.js
@@ -2,22 +2,20 @@ import isMobile from './is-mobile.js';
 
 if (typeof window !== 'undefined') {
   if (isMobile()) {
-    document.body.innerHTML = '';
+    document.documentElement.innerHTML = '';
     fetch('src/webpage/mobile/index.html')
       .then(response => response.text())
       .then(data => {
         document.body.innerHTML = data;
       })
       .catch(e => console.error(e));
-    document.head.title = 'MOBILE';
   } else {
-    document.body.innerHTML = '';
+    document.documentElement.innerHTML = '';
     fetch('src/webpage/desktop/index.html')
       .then(response => response.text())
       .then(data => {
         document.body.innerHTML = data;
       })
       .catch(e => console.error(e));
-    document.head.title = 'DESKTOP';
   }
 }
