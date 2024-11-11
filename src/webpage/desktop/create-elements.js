1d7f01a0853d4c3bc2c10ff58aa4d87aa429a6c9
electricprogramming <yeungj316@gmail.com>
2024-11-11 11:33:58 -0500

    rename some vars

--- src/webpage/desktop/create-elements.js
+++ src/webpage/desktop/create-elements.js
@@ -1,12 +1,12 @@
 import extensions from '../existing-extensions.js';
 import messages from '../message-system.js';
-const svgText = `
+const notFoundSvgText = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 400" width="600" height="400">
   <rect x="2.5" y="2.5" width="595" height="395" rx="40" ry="40" stroke="gray" stroke-width="5"/>
   <text x="300" y="210" font-size="100" fill="red" font-family="Arial" text-anchor="middle" dominant-baseline="middle">Not Found</text>
 </svg>
 `
-const notFoundSVG = `data:image/svg+xml,${encodeURIComponent(svgText)}`;
+const notFoundSvgUrl = `data:image/svg+xml,${encodeURIComponent(notFoundSvgText)}`;
 function createExtensionElements() {
   const extContainer = document.getElementById('extension-container');
   extensions.forEach(ext => {
@@ -14,7 +14,7 @@ function createExtensionElements() {
     extElement.src = `/src/extension-icons/${ext.path}.svg`;
     extElement.classList.add('extElement');
     extElement.onerror = function() {
-      this.src = notFoundSVG;
+      this.src = notFoundSvgUrl;
     };
     extElement.addEventListener('click', () => {
       messages.broadcast('EXTENSION_BUTTON', ext);
