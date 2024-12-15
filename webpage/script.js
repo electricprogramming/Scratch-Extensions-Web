7cdd89c4110722b193cafc1fcf9e8ae66aebe72f
electricprogramming <yeungj316@gmail.com>
2024-12-15 15:27:59 -0500

    open modal now

--- webpage/script.js
+++ webpage/script.js
@@ -5,6 +5,10 @@ import settings from './settings.js';
 import getExtFile from './get-ext-file.js';
 import downloadFile from './download-file.js';
 import getSearchResults from './get-search-results.js';
+import settings from './settings.js';
+const searchBar = document.getElementById('search-bar');
+const settingsButton = document.getElementById('settings-button');
+const settingsModal = document.getElementById('settings-modal');
 createExtensionElements();
 messages.on('EXTENSION_BUTTON', (ext) => {
   const extFile = getExtFile(ext.path);
@@ -17,7 +21,6 @@ messages.on('EXTENSION_BUTTON', (ext) => {
       break;
   }
 }, 'process-extension-clicked');
-const searchBar = document.getElementById('search-bar');
 searchBar.addEventListener('input', () => {
   const extElements = Array.from(document.querySelectorAll('.extElement')); // querySelectorAll returns a NodeList
   const searchResults = getSearchResults(extensions, searchBar.value).map(item => item.path);
@@ -30,7 +33,6 @@ searchBar.addEventListener('input', () => {
     }
   });
 });
-const settingsButton = document.getElementById('settings-button');
 settingsButton.addEventListener('click', () => {
-  alert('you clicked on the settings button');
+  settingsModal.style.display = 'flex';
 });
