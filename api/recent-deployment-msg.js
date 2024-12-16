be6d8be195adab73647927102396bd477003da5b
electricprogramming <yeungj316@gmail.com>
2024-12-16 11:58:13 -0500

    lol

--- api/recent-deployment-msg.js
+++ api/recent-deployment-msg.js
@@ -1,3 +1,34 @@
 export default function handler(req, res) {
-  return res.send('This is a work in progress. It is not complete yet.');
+  const VERCEL_API_KEY = process.env.VERCEL_API_KEY;
+  const PROJECT_NAME = "scratch-extensions-web";
+  const TEAM_ID = "team_UdiGAc5Yh2R4JswpV4P7hlsa";
+  const reqTimestamp = req.query.timestamp || (Date.now() - 200);
+  fetch(
+    `https://api.vercel.com/v6/now/deployments?teamId=${TEAM_ID}&projectId=${PROJECT_NAME}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${VERCEL_API_KEY}`,
+      },
+    }
+  )
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return data;
+    })
+    .then(data => data.deployments)
+    .then(deployments => {
+      let flag;
+      deployments.forEach((deployment) => {
+        if (deployment.state === "READY" && deployment.target === (req.get('referer').includes('https://clickylatin-dev.vercel.app')? null : "production") && deployment.ready < reqTimestamp - 2500) {
+          res.status(200).json({ message: deployment.meta.githubCommitMessage });
+          flag = true;
+          return;
+        }
+      });
+      if (!flag) res.status(404).json({ error: "not found" });
+    })
+    .catch((error) => res.status(500).json({ error }));
+  return res.status(200).send('This is a work in progress. It is not complete yet.');
 }
