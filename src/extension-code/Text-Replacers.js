18b949a0ae13cbd4c8e510a465bb73086c19d6e2
electricprogramming <yeungj316@gmail.com>
2025-08-07 16:10:50 -0400

    screw it

--- src/extension-code/Text-Replacers.js
+++ src/extension-code/Text-Replacers.js
@@ -21,7 +21,7 @@
   class ReplacersExt {
     getInfo() {
       return {
-        id: 'epTextReplacers',
+        id: 'epReplacers',
         name: 'Text Replacers',
         color1: '#59C059',
         get menuIconURI() {
