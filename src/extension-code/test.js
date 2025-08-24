5c9c58a46939b423c72a7f7ca918c4ae54fc7f02
electricprogramming <yeungj316@gmail.com>
2025-08-24 13:14:11 -0400

    BANG

--- src/extension-code/test.js
+++ src/extension-code/test.js
@@ -1,14 +1,24 @@
 (async function (Scratch) {
   'use strict';
-
   if (!Scratch.extensions.unsandboxed) {
     function xmlSafe(str) { return str.replace(/[\u0000-\u001F]/g, '�').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;') } function validId(str) { return str.replaceAll(/[^a-zA-Z0-9]/g, '') } class ErroredExtension { constructor(name) { this.name = name } getInfo() { return { id: `ERR${validId(this.name)}`, name: 'ERR', blocks: [{ blockType: Scratch.BlockType.XML, xml: `<label text="The ${xmlSafe(this.name)} extension"/><sep gap="-12"/><sep gap="12"/><sep gap="-10"/><label text="must run unsandboxed."/><sep gap="-12"/><sep gap="12"/>` },] } } }
     return Scratch.extensions.register(new ErroredExtension('Test Ext'));
   }
-  const { BlockType, ArgumentType, vm } = Scratch, runtime = vm.runtime;
 
-  const customFieldTypes = {};
-  let Blockly = null;
+  const { BlockType, ArgumentType, vm } = Scratch, runtime = vm.runtime;
+  const ScratchBlocks = window.ScratchBlocks || await Scratch.gui.getBlockly();
+
+  // Temporary fix for the annoying error:
+  // '<text> attribute x: Expected length, "NaN".'
+  const _setAttribute = SVGTextElement.prototype.setAttribute;
+  SVGTextElement.prototype.setAttribute = function(attr, val, ...args) {
+    if (String(val) === 'NaN' && (attr === 'x' || attr === 'y') && this.getAttribute('class') === 'blocklyText') {
+      const nattr = `MoreFieldsAttrErr${attr.toUpperCase()}`;
+      _setAttribute.call(this, nattr, `Attempted an illegal set on this text node. ${attr.toUpperCase()} was set to NaN.`);
+      return _setAttribute.call(this, attr, '0', ...args);
+    }
+    return _setAttribute.call(this, attr, val, ...args);
+  };
 
   const _LDC = function _LightenDarkenColor(col, amt) {
     const num = parseInt(col.replace('#', ''), 16);
@@ -47,89 +57,54 @@
     if (textNode) _setCssNattr(textNode, 'fill', '#FFFFFF');
   }
 
-  function removeAllEventListeners(element, deep) { 
-    const clone = element.cloneNode(deep); // removes from children too if deep
-    element.replaceWith(clone);
-    return clone;
-  }
+  ArgumentType.DUMMYLABEL = 'DummyLabel';
   
-  const toRegisterOnBlocklyGot = [];
+  class FieldDummyLabel extends ScratchBlocks.FieldTextInput {
+    constructor(opt_value) {
+      opt_value = ArgumentType.DUMMYLABEL;
+      super(opt_value);
+      this.addArgType('String');
+      this.addArgType(ArgumentType.DUMMYLABEL);
+    }
+    init(...initArgs) {
+      ScratchBlocks.FieldTextInput.prototype.init.call(this, ...initArgs);
+      this.textNode__ = this.sourceBlock_.svgPath_.parentNode.querySelector('g.blocklyEditableText text');
+      console.log(this)
+      if (!!this.textNode__ && this.sourceBlock_.parentBlock_) _fixColours.call(this, this.sourceBlock_.parentBlock_.colour_);
+    }
+    showEditor_() {}
+  }
+
+  const customFieldTypes = {
+    [ArgumentType.DUMMYLABEL]: {
+      output: ArgumentType.STRING,
+      outputShape: 2,
+      implementation: {
+        fromJson: () => new FieldDummyLabel()
+      }
+    }
+  }
 
   vm.addListener('EXTENSION_FIELD_ADDED', (fieldInfo) => {
-    if (Blockly) Blockly.Field.register(fieldInfo.name, fieldInfo.implementation);
-    else toRegisterOnBlocklyGot.push([fieldInfo.name, fieldInfo.implementation]);
+    ScratchBlocks.Field.register(fieldInfo.name, fieldInfo.implementation);
   });
 
-  ArgumentType.DUMMYLABEL = 'DummyLabel';
-
-  let implementations = {
-    FieldDummyLabel: null
-  };
-  customFieldTypes[ArgumentType.DUMMYLABEL] = {
-    output: ArgumentType.STRING,
-    outputShape: 2,
-    implementation: {
-      fromJson: () => new implementations.FieldDummyLabel()
+  const eventsOriginallyEnabled = ScratchBlocks.Events.isEnabled(), workspace = ScratchBlocks.getMainWorkspace();
+  ScratchBlocks.Events.disable();
+  if (workspace) {
+    if (vm.editingTarget) vm.emitWorkspaceUpdate();
+    const flyout = workspace.getFlyout();
+    if (flyout) {
+      const flyoutWorkspace = flyout.getWorkspace();
+      ScratchBlocks.Xml.clearWorkspaceAndLoadFromXml(
+        ScratchBlocks.Xml.workspaceToDom(flyoutWorkspace),
+        flyoutWorkspace
+      );
+      workspace.getToolbox().refreshSelection();
+      workspace.toolboxRefreshEnabled_ = true;
     }
-  };
-  // Main try thing
-  function tryUseScratchBlocks(_sb) {
-    Blockly = _sb;
-
-    // Temporary fix for the annoying error:
-    // '<text> attribute x: Expected length, "NaN".'
-    const _setAttribute = SVGTextElement.prototype.setAttribute;
-    SVGTextElement.prototype.setAttribute = function(attr, val, ...args) {
-      if (String(val) === 'NaN' && (attr === 'x' || attr === 'y') && this.getAttribute('class') === 'blocklyText') {
-        const nattr = `MoreFieldsAttrErr${attr.toUpperCase()}`;
-        _setAttribute.call(this, nattr, `Attempted an illegal set on this text node. ${attr.toUpperCase()} was set to NaN.`);
-        return _setAttribute.call(this, attr, '0', ...args);
-      }
-      return _setAttribute.call(this, attr, val, ...args);
-    };
-
-    implementations.FieldDummyLabel = class FieldDummyLabel extends Blockly.FieldTextInput {
-      constructor(opt_value) {
-        opt_value = ArgumentType.DUMMYLABEL;
-        super(opt_value);
-        this.addArgType('String');
-        this.addArgType(ArgumentType.DUMMYLABEL);
-      }
-      init(...initArgs) {
-        Blockly.FieldTextInput.prototype.init.call(this, ...initArgs);
-        this.textNode__ = this.sourceBlock_.svgPath_.parentNode.querySelector('g.blocklyEditableText text');
-        console.log(this)
-        if (!!this.textNode__ && this.sourceBlock_.parentBlock_) _fixColours.call(this, this.sourceBlock_.parentBlock_.colour_);
-      }
-      showEditor_() {}
-    }
-
-    while (toRegisterOnBlocklyGot.length > 0) {
-      const [name, impl] = toRegisterOnBlocklyGot.shift();
-      Blockly.Field.register(name, impl);
-    }
-
-    // Attempt to reload the workspace and what not.
-    // https://github.com/TurboWarp/addons/blob/tw/addons/custom-block-shape/update-all-blocks.js
-    const eventsOriginallyEnabled = Blockly.Events.isEnabled(), workspace = Blockly.getMainWorkspace();
-    Blockly.Events.disable();
-    if (workspace) {
-      if (vm.editingTarget) vm.emitWorkspaceUpdate();
-      const flyout = workspace.getFlyout();
-      if (flyout) {
-        const flyoutWorkspace = flyout.getWorkspace();
-        Blockly.Xml.clearWorkspaceAndLoadFromXml(
-          Blockly.Xml.workspaceToDom(flyoutWorkspace),
-          flyoutWorkspace
-        );
-        workspace.getToolbox().refreshSelection();
-        workspace.toolboxRefreshEnabled_ = true;
-      }
-    }
-    if (eventsOriginallyEnabled) Blockly.Events.enable();
   }
-
-  Scratch.gui.getBlockly().then((Blockly) => tryUseScratchBlocks(Blockly));
+  if (eventsOriginallyEnabled) ScratchBlocks.Events.enable();
 
   class epTesting {
     static get customFieldTypes() {
